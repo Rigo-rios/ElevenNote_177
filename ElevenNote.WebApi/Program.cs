2525f8f16b12f5565f6e18ae44011545d024e3b3
Rigo-rios <146138438+Rigo-rios@users.noreply.github.com>
2023-12-27 23:56:35 -0500

    Added DbContext Injections

--- ElevenNote.WebApi/Program.cs
+++ ElevenNote.WebApi/Program.cs
@@ -1,6 +1,27 @@
+using ElevenNote.Data;
+using ElevenNote.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Add connectioncstring and DbContext Setup
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<UserEntity>(options =>
+{
+    //Pass word Configuration optional
+    options.Password.RequiredLength = 5;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireDigit = true;
+    options.Password.RequireNonAlphanumeric = true; 
+})
+.AddRoles<IdentityRole<int>>() // ENable ROles, optional
+.AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
