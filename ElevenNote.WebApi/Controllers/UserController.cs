939fc5fe22c2ed0847eb22309a2b160276485893
Rigo-rios <146138438+Rigo-rios@users.noreply.github.com>
2024-01-04 17:09:51 -0500

    Added HttpGET userId

--- ElevenNote.WebApi/Controllers/UserController.cs
+++ ElevenNote.WebApi/Controllers/UserController.cs
@@ -31,6 +31,17 @@ namespace ElevenNote.WebApi.Controllers
             return BadRequest(new TextResponse("User could not be registered"));
         }
 
-        
+
+        [HttpGet("{userId:int}")]
+        public async Task<IActionResult> GetById([FromRoute] int userId)
+        {
+            UserDetail? detail = await _userService.GetUserIdAsync(userId);
+
+            if(detail is null)
+            {
+                return NotFound();
+            }
+            return Ok(detail);
+        }
     }
 }
